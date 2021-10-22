11ffd5de73822882d3e3a2b7d658a68267ec1c43
Dzmitry Liashko <dzliashko@gmail.com>
2021-10-22 20:30:11 +0300

    Add: vowelCount.js finished

--- vowelCount.js
+++ vowelCount.js
@@ -8,6 +8,28 @@ function getCount(str) {
   let vowelsCount = 0;
 
   // enter your majic here
-
+  for (let i = 0; i < str.length; i++) {
+    switch (str[i]) {
+      case "a":
+        vowelsCount++;
+        break;
+      case "e":
+        vowelsCount++;
+        break;
+      case "i":
+        vowelsCount++;
+        break;
+      case "o":
+        vowelsCount++;
+        break;
+      case "u":
+        vowelsCount++;
+        break;
+      default:
+        break;
+    }
+  }
   return vowelsCount;
 }
+
+console.log(getCount("abracadabra"));
