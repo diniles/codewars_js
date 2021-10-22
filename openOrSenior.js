766e7bdeb5e4115bd6294b3a44d994fb4fc8168b
Dzmitry Liashko <dzliashko@gmail.com>
2021-10-22 19:37:00 +0300

    Add: openOrSenior

---
+++ openOrSenior.js
@@ -0,0 +1,27 @@
+function openOrSenior(data) {
+  // ...
+  const result = [];
+  for (i = 0; i < data.length; i++) {
+    if (data[i][0] >= 55 && data[i][1] > 7) {
+      result.push("Senior");
+    } else {
+      result.push("Open");
+    }
+  }
+  return result;
+}
+
+console.log(
+  openOrSenior([
+    [75, 11],
+    [90, 9],
+    [90, 7],
+    [21, 21],
+    [60, 12],
+    [1, 1],
+    [90, 8],
+    [54, 9],
+    [55, 10],
+    [0, 0],
+  ])
+);
