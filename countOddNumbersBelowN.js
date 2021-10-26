51ea7044b744af0f99a15940ad69350c17b0d36d
Dzmitry Liashko <dzliashko@gmail.com>
2021-10-26 18:48:04 +0300

    Add: countOddNumbersBelowN.js finished

--- countOddNumbersBelowN.js
+++ countOddNumbersBelowN.js
@@ -1,10 +1,22 @@
+// countOddNumbersBelowN.js
 // Count Odd Numbers below n
 // Given a number n, return the number of positive odd numbers
 // below n, EASY!
 
-oddCount(7); //=> 3, i.e [1, 3, 5]
-oddCount(15); //=> 7, i.e [1, 3, 5, 7, 9, 11, 13]
+// oddCount(7); //=> 3, i.e [1, 3, 5]
+// oddCount(15); //=> 7, i.e [1, 3, 5, 7, 9, 11, 13]
 
 function oddCount(n) {
   // your code here
+  let count = 0;
+
+  for (let i = 1; i < n; i++) {
+    if (i % 2 != 0) {
+      count++;
+    }
+  }
+  return count;
 }
+
+console.log(oddCount(7));
+console.log(oddCount(15));
