6207914271db5ddbab5c9a90a0d1c79038baa503
Dzmitry Liashko <dzliashko@gmail.com>
2021-10-26 18:56:11 +0300

    Fix: fast solution for countOddNumbersBelowN.js

--- countOddNumbersBelowN.js
+++ countOddNumbersBelowN.js
@@ -5,18 +5,21 @@
 
 // oddCount(7); //=> 3, i.e [1, 3, 5]
 // oddCount(15); //=> 7, i.e [1, 3, 5, 7, 9, 11, 13]
+// oddCount(15023), 7511, "Oops! Wrong.");
 
 function oddCount(n) {
   // your code here
-  let count = 0;
+  // let count = 0;
 
-  for (let i = 1; i < n; i++) {
-    if (i % 2 != 0) {
-      count++;
-    }
-  }
-  return count;
+  // for (let i = 1; i < n; i++) {
+  //   if (i % 2 != 0) {
+  //     count++;
+  //   }
+  // }
+  return n % 2 != 0 ? (count = (n - 1) / 2) : (count = n / 2);
 }
 
 console.log(oddCount(7));
 console.log(oddCount(15));
+console.log(oddCount(15023));
+console.log(oddCount(1940314687537768));
