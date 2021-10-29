bd680fa9f9b93f9b3989649fc2760f2f3cad200c
diniles <dzliashko@gmail.com>
2021-10-29 14:59:44 +0300

    Add: myLanguages.js solved

--- Codewars1-2021Q3/Objects/myLanguages.js
+++ Codewars1-2021Q3/Objects/myLanguages.js
@@ -13,3 +13,21 @@
 // {"C++": 50, "ASM": 10, "Haskell": 20}     -->  []
 // My other katas
 // If you enjoyed this kata then please try my other katas! :-)
+
+function myLanguages(results) {
+  const sorted = Object.entries(results)
+    .sort(([, a], [, b]) => b - a)
+    .reduce((r, [k, v]) => ({ ...r, [k]: v }), {});
+
+  const langs = [];
+  for (const key in sorted) {
+    if (Number(sorted[key]) >= 60) {
+      langs.push(key);
+    }
+  }
+  return langs;
+}
+
+console.log(myLanguages({ Java: 10, Ruby: 80, Python: 65 }));
+console.log(myLanguages({ Hindi: 60, Dutch: 93, Greek: 71 }));
+console.log(myLanguages({ "C++": 50, ASM: 10, Haskell: 20 }));
