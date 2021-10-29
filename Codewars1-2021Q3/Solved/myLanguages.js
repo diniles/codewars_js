c54c3f55d4c1acd2ff3aae38f774cb1f71162e19
diniles <dzliashko@gmail.com>
2021-10-29 15:01:07 +0300

    Add: myLanguages.js best solution and finished

--- Codewars1-2021Q3/Solved/myLanguages.js
+++ Codewars1-2021Q3/Solved/myLanguages.js
@@ -28,6 +28,11 @@ function myLanguages(results) {
   return langs;
 }
 
+// best solution
+// function myLanguages(results) {
+//   return Object.keys(results).filter(r => results[r] > 59).sort((a,b) => results[b] - results[a]);
+// }
+
 console.log(myLanguages({ Java: 10, Ruby: 80, Python: 65 }));
 console.log(myLanguages({ Hindi: 60, Dutch: 93, Greek: 71 }));
 console.log(myLanguages({ "C++": 50, ASM: 10, Haskell: 20 }));
