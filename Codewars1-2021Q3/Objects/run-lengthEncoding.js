ec319ad0e308e52f9fa874a0e379c24cba6a66c5
Dzmitry Liashko <dzliashko@gmail.com>
2021-10-30 16:14:02 +0300

    Add: run-lengthEncoding.js solved

--- Codewars1-2021Q3/Objects/run-lengthEncoding.js
+++ Codewars1-2021Q3/Objects/run-lengthEncoding.js
@@ -23,3 +23,20 @@
 
 // runLengthEncoding("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbb")
 //  // => [[34,'a'], [3,'b']]
+
+function runLengthEncoding(str) {
+  const result = [];
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] == str[i - 1]) continue;
+    let count = 0;
+    let j = i;
+    while (str[j] === str[i]) {
+      count++;
+      j++;
+    }
+    result.push([count, str[i]]);
+  }
+  return result;
+}
+console.log(runLengthEncoding("hello world!"));
+console.log(runLengthEncoding("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbb"));
