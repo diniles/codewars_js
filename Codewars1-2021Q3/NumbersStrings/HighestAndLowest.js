a1bfafa034ed9cdcc8298f2b9a9d53fa0fc22f3e
diniles <dzliashko@gmail.com>
2021-11-02 15:38:27 +0300

    Fix: HighestAndLowest.js array sort

--- Codewars1-2021Q3/NumbersStrings/HighestAndLowest.js
+++ Codewars1-2021Q3/NumbersStrings/HighestAndLowest.js
@@ -14,7 +14,7 @@
 // number is first.
 
 function highAndLow(numbers) {
-  const arr = numbers.split(" ").sort();
+  const arr = numbers.split(" ").sort((a, b) => a - b);
   return arr[arr.length - 1] + " " + arr[0];
 }
 
