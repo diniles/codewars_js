1df537bd184e7eeb8e0570761b38ab03eedc88b3
diniles <dzliashko@gmail.com>
2021-11-02 15:37:00 +0300

    Add: HighestAndLowest.js test for numerical values

--- Codewars1-2021Q3/NumbersStrings/HighestAndLowest.js
+++ Codewars1-2021Q3/NumbersStrings/HighestAndLowest.js
@@ -21,3 +21,4 @@ function highAndLow(numbers) {
 console.log(highAndLow("1 2 3 4 5"));
 console.log(highAndLow("1 2 -3 4 5"));
 console.log(highAndLow("1 9 3 4 -5"));
+console.log(highAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4"));
