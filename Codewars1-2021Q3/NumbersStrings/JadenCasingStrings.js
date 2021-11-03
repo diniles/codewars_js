beb7e4aa2a1d6fcd6ffccac3942240d0e4e623f3
diniles <dzliashko@gmail.com>
2021-11-03 16:33:55 +0300

    Fix: JadenCasingStrings.js but still not working

--- Codewars1-2021Q3/NumbersStrings/JadenCasingStrings.js
+++ Codewars1-2021Q3/NumbersStrings/JadenCasingStrings.js
@@ -17,7 +17,9 @@
 // Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
 
 String.prototype.toJadenCase = function () {
-  return this.charAt(0).toUpperCase() + this.slice(1);
+  this.split(" ")
+    .map((word) => word.toJadenCase())
+    .join(" ");
 };
 
 console.log("How can mirrors be real if our eyes aren't real".toJadenCase());
