bac3561c806ce69d280fa6244e7d54cc761e8bd7
diniles <dzliashko@gmail.com>
2021-11-03 16:51:30 +0300

    Add: JadenCasingStrings.js finished

--- Codewars1-2021Q3/Solved/JadenCasingStrings.js
+++ Codewars1-2021Q3/Solved/JadenCasingStrings.js
@@ -25,4 +25,13 @@ String.prototype.toJadenCase = function () {
   return pieces.join(" ");
 };
 
+// best solution
+String.prototype.toJadenCase = function () {
+  return this.split(" ")
+    .map(function (word) {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    })
+    .join(" ");
+};
+
 console.log("How can mirrors be real if our eyes aren't real".toJadenCase());
