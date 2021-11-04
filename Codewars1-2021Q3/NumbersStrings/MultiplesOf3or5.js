bd80ee76e290575af04eb074eb5c72dadd23c7c1
diniles <dzliashko@gmail.com>
2021-11-04 11:34:47 +0300

    Add: MultiplesOf3or5.js solved

--- Codewars1-2021Q3/NumbersStrings/MultiplesOf3or5.js
+++ Codewars1-2021Q3/NumbersStrings/MultiplesOf3or5.js
@@ -8,3 +8,18 @@
 // return 0 (for languages that do have them).
 
 // Note: If the number is a multiple of both 3 and 5, only count it once.
+
+function solution(number) {
+  let result = 0;
+  if (number < 0) {
+    return result;
+  }
+  for (let i = 1; i < number; i++) {
+    if (i % 3 == 0 || i % 5 == 0) {
+      result += i;
+    }
+  }
+  return result;
+}
+
+console.log(solution(10));
