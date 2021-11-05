367d83f59a84983a0393ca31067b4ced6e332b69
Dzmitry Liashko <dzliashko@gmail.com>
2021-11-05 08:22:26 +0300

    Add: NicknameGenerator.js check name length

--- Codewars1-2021Q3/NumbersStrings/NicknameGenerator.js
+++ Codewars1-2021Q3/NumbersStrings/NicknameGenerator.js
@@ -26,6 +26,9 @@
 // The input can be modified
 
 function nickname(name) {
+  if (name.length < 4) {
+    return "Error: Name too short";
+  }
   const vowels = "aeiou";
   if (vowels.includes(name[2])) {
     return name.slice(0, 4);
