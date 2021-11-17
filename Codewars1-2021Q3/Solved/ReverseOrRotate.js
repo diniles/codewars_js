b4851610b4e250f8e027ef673821c2b5869d4a27
diniles <dzliashko@gmail.com>
2021-11-17 11:03:31 +0300

    Add: ReverseOrRotate.js finished & add best solution

--- Codewars1-2021Q3/Solved/ReverseOrRotate.js
+++ Codewars1-2021Q3/Solved/ReverseOrRotate.js
@@ -64,6 +64,21 @@ function revrot(str, sz) {
   return toChunks();
 }
 
+// best solution
+function revrot(str, sz) {
+  if (sz < 1 || sz > str.length)
+    return '';
+
+  let reverse = s => s.split('').reverse().join('');
+  let rotate  = s => s.slice(1) + s.slice(0, 1);
+  let sum_cubes = c => c.split('').reduce((a, b) => a + +b ** 3, 0);
+
+  return str
+    .match(new RegExp('.{' + sz + '}', 'g'))
+    .map(c => sum_cubes(c) % 2 ? rotate(c) : reverse(c))
+    .join('');
+}
+
 // console.log(revrot("123456987654", 6));
 // console.log(revrot("123456987653", 6));
 // console.log(revrot("66443875", 4));
