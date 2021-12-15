6278edb46a2aec2e17079503ebfa14b327ae4faf
Dzmitry Liashko <dzliashko@gmail.com>
2021-12-15 08:04:25 +0300

    Add: canWeDivideIt solved

--- Codewars1-2021Q3/_preschool-2022/canWeDivideIt.js
+++ Codewars1-2021Q3/_preschool-2022/canWeDivideIt.js
@@ -13,5 +13,12 @@
 // (15, -5, 3)   ->  true
 
 function isDivideBy(number, a, b) {
-  // good luck
+  return number % a == 0 && number % b == 0;
 }
+
+console.log(isDivideBy(-12, 2, -6));
+console.log(isDivideBy(-12, 2, -5));
+console.log(isDivideBy(45, 1, 6));
+console.log(isDivideBy(45, 5, 15));
+console.log(isDivideBy(4, 1, 4));
+console.log(isDivideBy(15, -5, 3));
