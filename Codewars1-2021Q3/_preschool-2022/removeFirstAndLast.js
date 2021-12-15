e5cd3d3e9d7c9c0d7a492fc956eb558e8a3b6010
diniles <dzliashko@gmail.com>
2021-12-15 08:48:39 +0300

    Add: removeFirstAndLast.js

---
+++ Codewars1-2021Q3/_preschool-2022/removeFirstAndLast.js
@@ -0,0 +1,14 @@
+// removeFirstAndLast.js
+// It's pretty straightforward. Your goal is to create a function that removes
+// the first and last characters of a string. You're given one parameter, the
+// original string. You don't have to worry with strings with less than two characters.
+
+assert.strictEqual(removeChar("eloquent"), "loquen");
+assert.strictEqual(removeChar("country"), "ountr");
+assert.strictEqual(removeChar("person"), "erso");
+assert.strictEqual(removeChar("place"), "lac");
+assert.strictEqual(removeChar("ooopsss"), "oopss");
+
+function removeChar(str) {
+  //You got this!
+}
