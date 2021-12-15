1a0ffe17e5d729ae64cbf6f27f7e0bf5e428f507
Dzmitry Liashko <dzliashko@gmail.com>
2021-12-15 17:31:43 +0300

    Add: fakeBinary.js submited & another solution

--- Codewars1-2021Q3/_preschool-2022/solved/fakeBinary.js
+++ Codewars1-2021Q3/_preschool-2022/solved/fakeBinary.js
@@ -15,6 +15,11 @@ function fakeBin(x) {
     .join("");
 }
 
+//another solution
+function fakeBin(x) {
+  return x.replace(/\d/g, (d) => (d < 5 ? 0 : 1));
+}
+
 console.log(fakeBin("45385593107843568"));
 console.log(fakeBin("509321967506747"));
 console.log(fakeBin("366058562030849490134388085"));
