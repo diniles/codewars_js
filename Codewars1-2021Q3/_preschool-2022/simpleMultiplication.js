dad321ed67d292bcbb054adad0c3647c52628320
diniles <dzliashko@gmail.com>
2021-12-15 08:44:40 +0300

    Add: simpleMultiplication.js

---
+++ Codewars1-2021Q3/_preschool-2022/simpleMultiplication.js
@@ -0,0 +1,11 @@
+// simpleMultiplication.js
+// This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+//     assert.strictEqual(simpleMultiplication(2),16,'Should return double given argument..')
+//     assert.strictEqual(simpleMultiplication(1),9,'Should return double given argument..')
+//     assert.strictEqual(simpleMultiplication(8),64,'Should return given argument times eight...')
+//     assert.strictEqual(simpleMultiplication(4),32,'Should return given argument times eight...')
+//     assert.strictEqual(simpleMultiplication(5),45,'Should return given argument times nine...')
+
+function simpleMultiplication(number) {
+  // your code........
+}
