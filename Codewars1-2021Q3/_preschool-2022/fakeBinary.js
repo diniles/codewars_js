27f727762f993208cad50cebd58c8454a9e7a803
Dzmitry Liashko <dzliashko@gmail.com>
2021-12-15 17:26:09 +0300

    Add: fakeBinary.js some tests

--- Codewars1-2021Q3/_preschool-2022/fakeBinary.js
+++ Codewars1-2021Q3/_preschool-2022/fakeBinary.js
@@ -9,3 +9,7 @@
 // assert.strictEqual(fakeBin('366058562030849490134388085'), '011011110000101010000011011');
 
 function fakeBin(x) {}
+
+console.log(fakeBin("45385593107843568"));
+console.log(fakeBin("509321967506747"));
+console.log(fakeBin("366058562030849490134388085"));
