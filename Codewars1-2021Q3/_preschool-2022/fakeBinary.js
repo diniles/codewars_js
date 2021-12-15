4577ea05af21dad550a8e10276feb98b90f00141
diniles <dzliashko@gmail.com>
2021-12-15 09:22:13 +0300

    Add: fakeBinary.js

---
+++ Codewars1-2021Q3/_preschool-2022/fakeBinary.js
@@ -0,0 +1,9 @@
+// fakeBinary.js
+// Given a string of digits, you should replace any digit below 5 with '0' and any
+// digit 5 and above with '1'. Return the resulting string.
+
+// assert.strictEqual(fakeBin('45385593107843568'), '01011110001100111');
+// assert.strictEqual(fakeBin('509321967506747'), '101000111101101');
+// assert.strictEqual(fakeBin('366058562030849490134388085'), '011011110000101010000011011');
+
+function fakeBin(x) {}
