93d178de44d4565dfab5511838469da659785861
diniles <dzliashko@gmail.com>
2021-12-15 09:20:59 +0300

    Add: largestSquareInsideCircle.js

---
+++ Codewars1-2021Q3/_preschool-2022/largestSquareInsideCircle.js
@@ -0,0 +1,10 @@
+// largestSquareInsideCircle.js
+// Determine the area of the largest square that can fit inside a circle with radius r.
+
+// assert.strictEqual(areaLargestSquare(5), 50);
+// assert.strictEqual(areaLargestSquare(7), 98);
+// assert.strictEqual(areaLargestSquare(15), 450);
+
+function areaLargestSquare(r) {
+  return; //code here
+}
