c1253db2eac0c644f980bc7e93e2eb4dfb3fac66
diniles <dzliashko@gmail.com>
2021-12-15 08:41:26 +0300

    Add: switchItUp.js

---
+++ Codewars1-2021Q3/_preschool-2022/switchItUp.js
@@ -0,0 +1,15 @@
+// switchItUp.js
+// When provided with a number between 0-9, return it in words.
+
+// Input :: 1
+
+// Output :: "One".
+
+// If your language supports it, try using a switch statement.
+//     assert.strictEqual(switchItUp(1),"One");
+//     assert.strictEqual(switchItUp(3),"Three");
+//     assert.strictEqual(switchItUp(5),"Five");
+
+function switchItUp(number) {
+  //Write your own Code!
+}
