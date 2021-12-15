edfb04e97569fd8c73139813787d345aeef18452
diniles <dzliashko@gmail.com>
2021-12-15 08:38:53 +0300

    Add: thirdAngleOfTriangle.js

---
+++ Codewars1-2021Q3/_preschool-2022/thirdAngleOfTriangle.js
@@ -0,0 +1,16 @@
+// thirdAngleOfTriangle.js
+// You are given two interior angles (in degrees) of a triangle.
+
+// Write a function to return the 3rd.
+
+// Note: only positive integers will be tested.
+
+// https://en.wikipedia.org/wiki/Triangle
+//     assert.strictEqual(otherAngle(30, 60), 90);
+//     assert.strictEqual(otherAngle(60, 60), 60);
+//     assert.strictEqual(otherAngle(43, 78), 59);
+//     assert.strictEqual(otherAngle(10, 20), 150);
+
+function otherAngle(a, b) {
+  return 0;
+}
