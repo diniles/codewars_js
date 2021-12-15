57a017214f3fe5534d6ab9cfce819e60fdffdb88
diniles <dzliashko@gmail.com>
2021-12-15 09:17:45 +0300

    Add: numberOfDecimal.js

---
+++ Codewars1-2021Q3/_preschool-2022/numberOfDecimal.js
@@ -0,0 +1,16 @@
+// numberOfDecimal.js
+// Determine the total number of digits in the integer (n>=0) given as input to
+// the function. For example, 9 is a single digit, 66 has 2 digits and 128685
+// has 6 digits. Be careful to avoid overflows/underflows.
+
+// describe("Solution", function(){
+//   it("Example tests", function(){
+//     Test.assertEquals(digits(5), 1, "Fail!");
+//     Test.assertEquals(digits(12345), 5, "Fail!");
+//     Test.assertEquals(digits(9876543210), 10, "Fail!");
+//   });
+// });
+
+function digits(n) {
+  // code goes here
+}
