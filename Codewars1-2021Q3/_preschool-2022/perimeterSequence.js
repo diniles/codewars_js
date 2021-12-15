158fd321c39d26d0bcfe29f230fdb3e1ab113a5e
diniles <dzliashko@gmail.com>
2021-12-15 08:50:27 +0300

    Add: perimeterSequence.js

---
+++ Codewars1-2021Q3/_preschool-2022/perimeterSequence.js
@@ -0,0 +1,21 @@
+// perimeterSequence.js
+// https://www.codewars.com/kata/589519d1f0902e01af000054/train/javascript
+
+// Perimeter sequence
+// The first three stages of a sequence are shown.
+
+// blocks
+
+// The blocksize is a by a and a ≥ 1.
+
+// What is the perimeter of the nth shape in the sequence (n ≥ 1) ?
+
+// describe("Tests", () => {
+//   it("test", () => {
+// Test.assertEquals(perimeterSequence(1,3), 12)
+//   });
+// });
+
+function perimeterSequence(a, n) {
+  // your code
+}
