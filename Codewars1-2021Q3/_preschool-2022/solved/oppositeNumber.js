98eb6f1714ca9da6a81d55010547c5b86cfe8f58
diniles <dzliashko@gmail.com>
2021-12-16 14:40:33 +0300

    Add: oppositeNumber.js submited & another solution

--- Codewars1-2021Q3/_preschool-2022/solved/oppositeNumber.js
+++ Codewars1-2021Q3/_preschool-2022/solved/oppositeNumber.js
@@ -13,6 +13,11 @@ function opposite(number) {
   //your code here
   return number * -1;
 }
+
+//another solution
+function opposite(number) {
+  return -number;
+}
 console.log(opposite(1));
 console.log(opposite(14));
 console.log(opposite(-34));
