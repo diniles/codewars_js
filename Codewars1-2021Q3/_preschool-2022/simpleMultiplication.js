d5367ddec24d4db94d4d84d082534215d61e4d1d
diniles <dzliashko@gmail.com>
2021-12-17 16:33:55 +0300

    Add: simpleMultiplication.js solved

--- Codewars1-2021Q3/_preschool-2022/simpleMultiplication.js
+++ Codewars1-2021Q3/_preschool-2022/simpleMultiplication.js
@@ -10,4 +10,11 @@
 
 function simpleMultiplication(number) {
   // your code........
+  return number % 2 == 0 ? number * 8 : number * 9;
 }
+
+console.log(simpleMultiplication(2));
+console.log(simpleMultiplication(1));
+console.log(simpleMultiplication(8));
+console.log(simpleMultiplication(4));
+console.log(simpleMultiplication(5));
