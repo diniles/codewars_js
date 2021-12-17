9f4b04262c24910779ce4987cb78c072ecde90e6
diniles <dzliashko@gmail.com>
2021-12-17 13:07:41 +0300

    Add: thinkfulLogicDrills.js solved

--- Codewars1-2021Q3/_preschool-2022/thinkfulLogicDrills.js
+++ Codewars1-2021Q3/_preschool-2022/thinkfulLogicDrills.js
@@ -16,4 +16,19 @@
 
 function updateLight(current) {
   //your code here!
+  switch (current) {
+    case "green":
+      return "yellow";
+      break;
+    case "yellow":
+      return "red";
+      break;
+    case "red":
+      return "green";
+      break;
+  }
 }
+
+console.log(updateLight("green"));
+console.log(updateLight("yellow"));
+console.log(updateLight("red"));
