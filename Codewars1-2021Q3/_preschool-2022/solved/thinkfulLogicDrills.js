6dda4463602c47f8283c9a2bd69eb739ee4bcfb4
diniles <dzliashko@gmail.com>
2021-12-17 13:08:39 +0300

    Add: thinkfulLogicDrills.js submited & another solution

--- Codewars1-2021Q3/_preschool-2022/solved/thinkfulLogicDrills.js
+++ Codewars1-2021Q3/_preschool-2022/solved/thinkfulLogicDrills.js
@@ -29,6 +29,15 @@ function updateLight(current) {
   }
 }
 
+// another solution
+function updateLight(current) {
+  return current === "yellow"
+    ? "red"
+    : current === "green"
+    ? "yellow"
+    : "green";
+}
+
 console.log(updateLight("green"));
 console.log(updateLight("yellow"));
 console.log(updateLight("red"));
