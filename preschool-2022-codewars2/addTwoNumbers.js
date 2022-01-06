2cc64ff0c2b4c6c977914e56f8e0b162ae921986
diniles <dzliashko@gmail.com>
2022-01-06 14:56:03 +0300

    feat: solved

--- preschool-2022-codewars2/addTwoNumbers.js
+++ preschool-2022-codewars2/addTwoNumbers.js
@@ -13,5 +13,16 @@
 // See link for explanation!!!
 
 function add(num1, num2) {
-  return num1 + num2;
+  num1 = ('' + num1).split('').reverse();
+  num2 = ('' + num2).split('').reverse();
+  let max = Math.max(num1.length, num2.length);
+  let str = [];
+  for (let i = 0; i < max; i++)
+    str.push((parseInt(num1[i]) || 0) + (parseInt(num2[i]) || 0));
+  num1 = str.reverse().join('');
+  return parseInt(num1);
 }
+
+console.log(add(16, 18));
+console.log(add(26, 39));
+console.log(add(122, 81));
