ff0057cd8a1286f11e6574ee7c1a95210249b8c8
diniles <dzliashko@gmail.com>
2022-01-07 12:37:41 +0300

    feat: powerOfTwo.js solved

--- preschool-2022-codewars2/powerOfTwo.js
+++ preschool-2022-codewars2/powerOfTwo.js
@@ -20,4 +20,9 @@
 
 function isPowerOfTwo(n) {
   //.. should return true or false ..
+  return n && ((n & (n - 1)) == 0);
 }
+
+console.log(isPowerOfTwo(1024));
+console.log(isPowerOfTwo(4096));
+console.log(isPowerOfTwo(333));
