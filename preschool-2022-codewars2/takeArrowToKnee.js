7f6ba7d3d98d105e721380f7149c9cae0c31dd35
diniles <dzliashko@gmail.com>
2022-01-13 11:30:05 +0300

    feat: takeArrowToKnee.js solved

--- preschool-2022-codewars2/takeArrowToKnee.js
+++ preschool-2022-codewars2/takeArrowToKnee.js
@@ -21,6 +21,9 @@
 // Test.assertEquals(ArrowFunc([70,85,83,32,82,79,72,32,68,65,72]), 'FUS ROH DAH', 'Keep it up!')
 
 
-var ArrowFunc = function(arr) {
-  return arr.map( => ).join(''); //Complete this function
-}
+const ArrowFunc = function (arr) {
+  return arr.map(el => String.fromCharCode(el)).join(''); //Complete this function
+};
+
+console.log(ArrowFunc([84, 101, 115, 116]));
+console.log(ArrowFunc([70, 85, 83, 32, 82, 79, 72, 32, 68, 65, 72]));
