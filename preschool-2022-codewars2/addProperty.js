cee4703e05c1d257956ed7c0b885794c7f01b5f7
diniles <dzliashko@gmail.com>
2022-01-18 15:14:11 +0300

    feat: addProperty.js solved

--- preschool-2022-codewars2/addProperty.js
+++ preschool-2022-codewars2/addProperty.js
@@ -40,3 +40,20 @@
 // });
 
 // no given function name!!!
+
+var questions = [{
+  question: "What's the currency of the USA?",
+  choices: ["US dollar", "Ruble", "Horses", "Gold"],
+  corAnswer: 0
+}, {
+  question: "Where was the American Declaration of Independence signed?",
+  choices: ["Philadelphia", "At the bottom", "Frankie's Pub", "China"],
+  corAnswer: 0
+}];
+
+questions.map(obj => {
+  obj.usersAnswer = null;
+});
+
+
+console.log(questions);
