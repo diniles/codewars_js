259345743a9ce868350cd8f3f8ec06b5a4085d73
Dzmitry Liashko <dzliashko@gmail.com>
2022-01-18 19:52:34 +0300

    feat: unpackingArguments.js solved

--- preschool-2022-codewars2/unpackingArguments.js
+++ preschool-2022-codewars2/unpackingArguments.js
@@ -13,5 +13,14 @@
 // someFunction(1, true, "Foo", "bar")
 
 function spread(func, args) {
-  // ...
+  return func(...args);
 }
+
+console.log(
+  spread(
+    function (x, y) {
+      return x + y;
+    },
+    [1, 2]
+  )
+);
