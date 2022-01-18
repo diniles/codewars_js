2b15a8f89b941e018a3c86f245ffbc668413b599
Dzmitry Liashko <dzliashko@gmail.com>
2022-01-18 20:10:04 +0300

    feat: arrowSums.js solved

--- preschool-2022-codewars2/arrowSums.js
+++ preschool-2022-codewars2/arrowSums.js
@@ -7,7 +7,7 @@
 // in an array using the Arrow style function
 // NOTE:
 
-// Because eval hasnt caught up and cannot evaluate arrow style functions I have
+// Because eval hasn't caught up and cannot evaluate arrow style functions I have
 // to do it manually. Your solution must contain the Arrow style function, the
 // curly braces inside the reduce and the return statement because it must be
 // manually converted to an old function style to test the validlity of the
@@ -17,4 +17,12 @@
 
 // Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
 
-var Sum = "sum = function(arr){ return arr.reduce( => ); }";
+var Sum = "sum = function(arr){ return arr.reduce( (a,b)=> a+b); }";
+
+var test = [
+  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+  [71, -548, 12.3, 83, -47, -1.7, -892, 58, 31415, 2718, -38],
+];
+
+console.log(Sum);
+console.log(Sum);
