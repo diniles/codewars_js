f2fdf83873d0bd158a1d4feddbb1832a3c5f1832
Dzmitry Liashko <dzliashko@gmail.com>
2024-10-29 16:36:31 +0300

    Add: ConsecutiveStrings

---
+++ other/ConsecutiveStrings.js
@@ -0,0 +1,89 @@
+// https://www.codewars.com/kata/56a5d994ac971f1ac500003e/train/javascript
+
+// You are given an array(list) strarr of strings and an integer k. Your task
+// is to return the first longest string consisting of k consecutive strings
+// taken in the array.
+
+// Examples:
+
+// strarr = ["tree", "foling", "trashy", "blue", "abcdef", "uvwxyz"], k = 2
+
+// Concatenate the consecutive strings of strarr by 2, we get:
+
+// treefoling   (length 10)  concatenation of strarr[0] and strarr[1]
+// folingtrashy ("      12)  concatenation of strarr[1] and strarr[2]
+// trashyblue   ("      10)  concatenation of strarr[2] and strarr[3]
+// blueabcdef   ("      10)  concatenation of strarr[3] and strarr[4]
+// abcdefuvwxyz ("      12)  concatenation of strarr[4] and strarr[5]
+
+// Two strings are the longest: "folingtrashy" and "abcdefuvwxyz".
+// The first that came is "folingtrashy" so
+// longest_consec(strarr, 2) should return "folingtrashy".
+
+// In the same way:
+// longest_consec(["zone", "abigail", "theta", "form", "libe", "zas", "theta",
+// "abigail"], 2) --> "abigailtheta"
+
+// n being the length of the string array, if n = 0 or k > n or k <= 0 return
+// "" (return Nothing in Elm, "nothing" in Erlang).
+
+// Note
+// consecutive strings : follow one after another without an interruption
+
+function longestConsec(strarr, k) {
+  const length = strarr.length;
+  if (length == 0 || k > length || k <= 0) {
+    return '';
+  }
+  if (k == 1) {
+    return strarr.sort((a, b) => b.length - a.length)[0];
+  }
+
+  let longest = '';
+  for (let i = 0; i <= strarr.length - k; i++) {
+    let tempStr = strarr.slice(i, i + k).join('');
+    if (tempStr.length > longest.length) {
+      longest = tempStr;
+    }
+  }
+  return longest;
+}
+
+console.log(
+  longestConsec(['zone', 'abigail', 'theta', 'form', 'libe', 'zas'], 2)
+); //abigailtheta
+
+console.log(
+  longestConsec(
+    [
+      'ejjjjmmtthh',
+      'zxxuueeg',
+      'aanlljrrrxx',
+      'dqqqaaabbb',
+      'oocccffuucccjjjkkkjyyyeehh',
+    ],
+    1
+  )
+); //oocccffuucccjjjkkkjyyyeehh
+console.log(longestConsec([], 3));
+console.log(
+  longestConsec(
+    [
+      'itvayloxrp',
+      'wkppqsztdkmvcuwvereiupccauycnjutlv',
+      'vweqilsfytihvrzlaodfixoyxvyuyvgpck',
+    ],
+    2
+  )
+); //wkppqsztdkmvcuwvereiupccauycnjutlvvweqilsfytihvrzlaodfixoyxvyuyvgpck
+console.log(
+  longestConsec(['wlwsasphmxx', 'owiaxujylentrklctozmymu', 'wpgozvxxiu'], 2)
+); //wlwsasphmxxowiaxujylentrklctozmymu
+console.log(
+  longestConsec(['zone', 'abigail', 'theta', 'form', 'libe', 'zas'], -2)
+);
+console.log(longestConsec(['it', 'wkppv', 'ixoyx', '3452', 'zzzzzzzzzzzz'], 3)); //ixoyx3452zzzzzzzzzzzz
+console.log(
+  longestConsec(['it', 'wkppv', 'ixoyx', '3452', 'zzzzzzzzzzzz'], 15)
+);
+console.log(longestConsec(['it', 'wkppv', 'ixoyx', '3452', 'zzzzzzzzzzzz'], 0));
